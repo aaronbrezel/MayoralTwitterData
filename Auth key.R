cddc8fff61ce4c12997329803439f2a2bff68dbd
JonahK-B <36051322+JonahK-B@users.noreply.github.com>
2018-03-27 14:15:40 -0500

    moved function

--- Auth key.R
+++ Auth key.R
@@ -1,7 +1,2 @@
 library(twitteR)
 setup_twitter_oauth("lmDdwehy7ZnAUPh5AH6Y3fbAR","2A62GUU7eshn5lZ3rHZ0MsepkFgbwG4qegMya4AyoydEmFxKQa")
-
-
-twitterStatusDF <- function(handle){
-list1 <<- twListToDF(userTimeline('handle', n=3200, includeRts = TRUE, excludeReplies = FALSE))
-  }
