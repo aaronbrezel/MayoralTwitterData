ab35b98073a5e0126fa394cf66a2fbd6833eea6a
JonahK-B <36051322+JonahK-B@users.noreply.github.com>
2018-03-29 02:35:01 -0500

    Works slightly better
    Realized how the handle needs to be fed into the function. Should work for lists of handles but is very slow for more than 4

--- Basic Function.R
+++ Basic Function.R
@@ -1,4 +1,5 @@
 twitterStatusDF <- function(handle){
- return(twListToDF(userTimeline('handle', n=3200, includeRts = TRUE, excludeReplies = FALSE)))
+ return(twListToDF(userTimeline(handle, n=3200, includeRts = TRUE, excludeReplies = FALSE)))
 }
 
+listSet <- sapply(c("realmaxhandler", "Klein_Barton"), twitterStatusDF)
