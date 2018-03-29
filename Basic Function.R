c9a53a5c4c35ae7f3f045c7b559e96aa8f5e8f16
JonahK-B <36051322+JonahK-B@users.noreply.github.com>
2018-03-29 14:11:26 -0500

    Importing twit handles

--- Basic Function.R
+++ Basic Function.R
@@ -1,5 +1,15 @@
-twitterStatusDF <- function(handle){
- return(twListToDF(userTimeline(handle, n=3200, includeRts = TRUE, excludeReplies = FALSE)))
+library(twitteR)
+setup_twitter_oauth("lmDdwehy7ZnAUPh5AH6Y3fbAR","2A62GUU7eshn5lZ3rHZ0MsepkFgbwG4qegMya4AyoydEmFxKQa")
+
+statusCollector <- function(handle){
+  return(twListToDF(userTimeline(handle, n=3200, includeRts = TRUE, excludeReplies = FALSE)))
 }
 
-listSet <- sapply(c("realmaxhandler", "Klein_Barton"), twitterStatusDF)
+twitterStatusDF <- function(handles){
+ twitDF <- as.data.frame(sapply(handles, statusCollector))
+ return(twitDF)
+}
+mayoralData  <- read.csv("Official Mayors List.csv", na.strings = c("N/A", NA))
+mayoralHandlesNull <- as.vector(mayoralData$Twitter_handle)
+mayoralHandles <- mayoralHandlesNull[!is.na(mayoralHandlesNull)]
+attempt <- twitterStatusDF(c('MartyHandlon','MayorHam', 'WoodfinForBham'))
