8b6719f536cb255bf1050fef0e74a537e8c7e83e
JonahK-B <36051322+JonahK-B@users.noreply.github.com>
2018-04-24 13:38:58 -0500

    Changed all plots to use mayor_Covariates

--- Basic info plots.R
+++ Basic info plots.R
@@ -23,25 +23,29 @@ userTable <- function(userHandles) { #userTable function to grab mayor twitter p
 profiles <- sapply(mayoralHandles, userTable)
 profiles <- rbindlist(profiles) #rbind for a df
 
-ggplot(data=profiles)+geom_bar(mapping = aes(x = verified)) ## Plot of how many accounts are verified vs. unverified
+ggplot(data=mayor_Covariates)+geom_bar(mapping = aes(x = verified)) ## Plot of how many accounts are verified vs. unverified
 
-ggplot(data=profiles)+geom_bar(mapping = aes(x = lang)) ## Mayors default language settings
+ggplot(data=mayor_Covariates)+geom_bar(mapping = aes(x = lang)) ## Mayors default language settings
+
+count(mayor_Covariates$lang == "es")
 
 ##Followercount plots
 
   #Followers between 0 and 10000
-ggplot(data = profiles) +
+ggplot(data = mayor_Covariates) +
   geom_histogram(mapping = aes(x = followersCount), breaks = seq(0, 10000, by = 100))
 
   #More than 10000 followers
-ggplot(data = profiles) +
+ggplot(data = mayor_Covariates) +
   geom_histogram(mapping = aes(x = followersCount), breaks = seq(10000, 300000, by = 10000))
-count(profiles$followersCount > 10000)
 
-ggplot(data = profiles, mapping = aes(x = statusesCount, y = followersCount)) + geom_point(na.rm = TRUE)
+
+#### REAL NICE LOOKING PLOT
+ggplot(data = mayor_Covariates, mapping = aes(x = log(statusesCount), y = log(followersCount))) + geom_point(na.rm = TRUE)
 
 
 ggplot(data = mayor_Covariates, mapping = aes(x = totalPopulation, y = followersCount)) + geom_point(na.rm = TRUE)
 
-ggplot(data = mayor_Covariates, mapping = aes(x = totalPopulation, y = followersCount)) + geom_point(na.rm = TRUE, alpha = 1/5)
-  
+ggplot(data = mayor_Covariates, mapping = aes(x = log(totalPopulation), y = log(followersCount))) + geom_point(na.rm = TRUE, alpha = 1/5)
+
+       
